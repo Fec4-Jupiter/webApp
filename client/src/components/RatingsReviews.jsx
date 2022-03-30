8a7ad584f080ec3f61b1f69482f3abb33bc70a9b
Tom Wheeler <ttwheelerb@gmail.com>
2022-03-30 12:00:26 -0500

    fixed base components

--- client/src/components/RatingsReviews.jsx
+++ client/src/components/RatingsReviews.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const RatingsReviews = () => (
-  <div>Overview</div>
+  <div>RatingsReviews</div>
 )
 
 export default RatingsReviews;
