b7aaea3bb044e87d51ed00409adb4be5ac2f71b7
Tom Wheeler <ttwheelerb@gmail.com>
2022-03-30 17:05:49 -0500

    finish implementing CircleCI tests, update folder structure for wireframes

--- tests/failingTest.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Idex test suite', () => {
-  it('False test should fail', () => {
-    expect(false).toEqual(true);
-  });
-});
-
-//a little change
---
+++ tests/template.spec.js
@@ -0,0 +1,9 @@
+//Copy this test template to add new tests.
+//Add as many tests per file as you want, but it
+//absolutely MUST end in .spec.js or it will not run
+
+describe('Index test suite', () => {
+  it('Truth test should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
