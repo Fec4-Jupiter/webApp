10f998d585f4f51857b20a16e8fbedd5e1cc0ad1
Tom Wheeler <ttwheelerb@gmail.com>
2022-03-31 14:40:55 -0500

    add React component files for Overview and Related items

---
+++ client/src/components/Overview/ProductInformation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProductInformation() {
+  return <div>ProductInformation</div>;
+}
+
+export default ProductInformation;
---
+++ client/src/components/RelatedItems/OutfitList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OutfitList() {
+  return <div>OutfitList</div>;
+}
+
+export default OutfitList;
