028af92435f861bc3b916612beddd0ca40794573
Tom Wheeler <ttwheelerb@gmail.com>
2022-04-01 10:30:25 -0500

    build review stars component

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import ReviewStars from './Common/ReviewStars.jsx';
+
+const reviews = [{
+  review_id: 1155753, rating: 5, summary: 'This product was great!', recommend: true, response: '', body: 'I really did or did not like this product based on whether it was sustainably sourced. Then I found out that its made from nothing at all.', date: '2019-01-01T00:00:00.000Z', reviewer_name: 'funtime', helpfulness: 8, photos: [],
+}, {
+  review_id: 1155754, rating: 4, summary: 'This product was ok!', recommend: false, response: '', body: 'I really did not like this product solely because I am tiny and do not fit into it.', date: '2019-01-11T00:00:00.000Z', reviewer_name: 'mymainstreammother', helpfulness: 2, photos: [],
+},
+];
 
 function Overview({ currentProduct }) {
-  return <div>Overview</div>;
+  return (
+    <div>
+      Overview
+      <ReviewStars reviews={reviews} />
+    </div>
+  );
 }
 
 export default Overview;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,20 +4,20 @@ import Overview from './components/Overview.jsx';
 import QuestionsAnswers from './components/QuestionsAnswers.jsx';
 import RatingsReviews from './components/RatingsReviews.jsx';
 import RelatedItems from './components/RelatedItems.jsx';
+const axios = require('axios');
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentProduct: {
-        id: 1,
-        name: 'Camo Onesie',
-        slogan: 'Blend in to your crowd',
-        description: 'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.',
-        category: 'Jackets',
-        default_price: '140',
-      },
+      currentProduct: {},
     };
+  }
+
+  componentDidMount() {
+    axios.get('/products')
+      .then((results) => this.setState({ currentProduct: results.data[0] }))
+      .catch((err) => { throw err; });
   }
 
   render() {
