6af541851081a1da6198e497c36e4ba2fc44f8c4
Marilene Soares da Costa <lene626@gmail.com>
2022-04-04 14:00:59 -0600

    updated QA component and added spec

--- client/src/components/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers.jsx
@@ -7,57 +7,96 @@ const axios = require('axios');
 class QuestionsAnswers extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      answers: [],
+    };
 
     this.fetchAnswers = this.fetchAnswers.bind(this);
-  }
-
-
-  fetchAnswers(questions) {
-    // for each question_id, generate a get call
-    // GET /qa/questions/:question_id/answers
-    let newState = {};
-    const answersList = questions.map((question) => {
-      return  axios.get(`/qa/questions/${question.question_id}/answers`)
-    });
-    Promise.all(answersList)
-    .then((values) => {
-      // setState has raw responses for now
-      this.setState(values);
-    })
-    .catch((err) => {
-      throw err;
-    })
   }
 
   componentDidMount() {
     this.fetchAnswers(this.props.questions);
   }
 
+  fetchAnswers(questions) {
+    // for each question_id, generate a get call
+    // GET /qa/questions/:question_id/answers
+    const newState = {};
+    const answersList = questions.map((question) => axios.get(`/qa/questions/${question.question_id}/answers`));
+    Promise.all(answersList)
+      .then((values) => {
+        newState.answers = values;
+        this.setState(newState);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   render() {
     return (
-      <div>
+      <div className="questionsandanswers">
         <h3> Questions and Answers Component</h3>
         <div>
-          <p> Product ID: {this.props.product.id}</p>
-          <h4>Questions:</h4>
+          <h4>
+            {' '}
+            Product ID:
+            {' '}
+            {this.props.product.id}
+          </h4>
+          <h4> ==== Questions</h4>
+          {this.props.questions.map((question) => (
+            <div key={question.question_id}>
+              <p>
+                {' '}
+                Question ID:
+                {question.question_id}
+              </p>
+              <p>
+                {' '}
+                Question:
+                {question.question_body}
+              </p>
+            </div>
+          ))}
           <div>
-            {this.props.questions.map((question) => {
-              return (
-                <div key={question.question_id}> Question: {question.question_body}</div>
-              )
-            })
-            }</div>
-
+            <h4> ==== Answers:</h4>
+            {this.state.answers.map((question) => {
+              if (question.data.results.length !== 0) {
+                return (
+                  question.data.results.map((answer) => (
+                    <div key={answer.answer_id}>
+                      <p>
+                        Answerer:
+                        {answer.answerer_name}
+                      </p>
+                      <p>
+                        Answer:
+                        {answer.body}
+                      </p>
+                      <p>
+                        Date:
+                        {answer.date}
+                      </p>
+                      <p>
+                        Helpfulness:
+                        {answer.helpfulness}
+                      </p>
+                    </div>
+                  ))
+                );
+              }
+            })}
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 QuestionsAnswers.propTypes = {
   product: PropTypes.instanceOf(Object),
-  questions: PropTypes.instanceOf(Object)
-}
+  questions: PropTypes.instanceOf(Object),
+};
 
 export default QuestionsAnswers;
---
+++ tests/QuestionsAndAnswers.spec.jsx
@@ -0,0 +1,22 @@
+import QuestionsAnswers from '..client/src/components/QuestionsAnswers.jsx';
+
+describe('QuestionsAnswers', function() {
+  var {
+    Simulate,
+    renderIntoDocument,
+    findRendereDOMComponentWithClass,
+    scryRenderedDOMComponentsWithClass
+  } = ReactTestUtils;
+
+  var questionsanswers;
+
+  beforeEach(function() {
+    questionsandanswers = renderIntoDocument(<QuestionsAnswers />);
+  });
+
+  it('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(QuestionsAnswers)).toEqual(true);
+  })
+
+
+});
