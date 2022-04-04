d9ec484efdaca8ee38a4030f80b36d503e5474e4
Tom Wheeler <ttwheelerb@gmail.com>
2022-04-04 15:47:36 -0500

    added initial tests for Overview component and ReviewStars component

--- tests/testdata.js
+++ tests/testdata.js
@@ -106,7 +106,13 @@ const testData = {
       product: '66642',
       page: 0,
       count: 5,
-      results: [],
+      results: [{
+        review_id: 1155762, rating: 2, summary: 'These pants are ok!', recommend: false, response: '', body: 'A little tight on the waist.', date: '2019-01-05T00:00:00.000Z', reviewer_name: 'bigbrother', helpfulness: 2, photos: [{ id: 2219425, url: 'https://images.unsplash.com/photo-1560829675-11dec1d78930?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80' }, { id: 2219426, url: 'https://images.unsplash.com/photo-1549812474-c3cbd9a42eb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' }, { id: 2219427, url: 'https://images.unsplash.com/photo-1559709319-3ae960cda614?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' }],
+      }, {
+        review_id: 1155761, rating: 5, summary: 'These pants are great!', recommend: true, response: '', body: 'I really like these pants. Best fit ever!', date: '2019-02-18T00:00:00.000Z', reviewer_name: 'figuringitout', helpfulness: 2, photos: [{ id: 2219422, url: 'https://images.unsplash.com/photo-1542574621-e088a4464f7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3028&q=80' }, { id: 2219423, url: 'https://images.unsplash.com/photo-1560294559-1774a164fb0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' }, { id: 2219424, url: 'https://images.unsplash.com/photo-1555689502-c4b22d76c56f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' }],
+      }, {
+        review_id: 1155760, rating: 4, summary: 'These pants are fine', recommend: true, response: '', body: 'I do like these pants', date: '2019-03-21T00:00:00.000Z', reviewer_name: 'shopaddict', helpfulness: 2, photos: [],
+      }],
     },
   },
   related: [66643, 66644, 66649, 66648],
