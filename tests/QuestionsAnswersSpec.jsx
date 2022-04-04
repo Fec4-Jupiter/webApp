40c4230cda92d369f7a0d0a5525cd43f51ec1f07
Marilene Soares da Costa <lene626@gmail.com>
2022-04-04 12:12:43 -0600

    adds a simple test spec

--- tests/QuestionsAnswersSpec.jsx
+++ tests/QuestionsAnswersSpec.jsx
@@ -14,5 +14,9 @@ describe('QuestionsAnswers', function() {
     questionsandanswers = renderIntoDocument(<QuestionsAnswers />);
   });
 
+  it('should be a stateful class component', function() {
+    expect(React.Component.isPrototypeOf(QuestionsAnswers)).toEqual(true);
+  })
+
 
 });
