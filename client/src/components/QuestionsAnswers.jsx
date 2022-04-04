ee047d51c074586e07f65d14080441cf7d0d792e
Marilene Soares da Costa <lene626@gmail.com>
2022-04-04 13:30:40 -0600

    fixes some linter issues with QA

--- client/src/components/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers.jsx
@@ -8,33 +8,29 @@ class QuestionsAnswers extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      answers: []
+      answers: [],
     };
 
     this.fetchAnswers = this.fetchAnswers.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchAnswers(this.props.questions);
+  }
 
   fetchAnswers(questions) {
     // for each question_id, generate a get call
     // GET /qa/questions/:question_id/answers
-    let newState = {};
-    const answersList = questions.map((question) => {
-      return axios.get(`/qa/questions/${question.question_id}/answers`)
-    });
+    const newState = {};
+    const answersList = questions.map((question) => axios.get(`/qa/questions/${question.question_id}/answers`));
     Promise.all(answersList)
       .then((values) => {
         newState.answers = values;
         this.setState(newState);
-
       })
       .catch((err) => {
         throw err;
-      })
-  }
-
-  componentDidMount() {
-    this.fetchAnswers(this.props.questions);
+      });
   }
 
   render() {
@@ -42,51 +38,65 @@ class QuestionsAnswers extends React.Component {
       <div className="questionsandanswers">
         <h3> Questions and Answers Component</h3>
         <div>
-          <h4> Product ID: {this.props.product.id}</h4>
+          <h4>
+            {' '}
+            Product ID:
+            {' '}
+            {this.props.product.id}
+          </h4>
           <h4> ==== Questions</h4>
-          {this.props.questions.map((question) => {
-            return (
-              <div key={question.question_id}>
-                <p> Question ID:  {question.question_id}</p>
-                <p> Question: {question.question_body} </p>
-              </div>
-            )
-          })
-          }
+          {this.props.questions.map((question)(
+            <div key={question.question_id}>
+              <p>
+                {' '}
+                Question ID:
+                {question.question_id}
+              </p>
+              <p>
+                {' '}
+                Question:
+                {question.question_body}
+              </p>
+            </div>
+          ))}
           <div>
             <h4> ==== Answers:</h4>
             {this.state.answers.map((question) => {
               if (question.data.results.length !== 0) {
                 return (
-                  question.data.results.map((answer) => {
-                    return (
-                      <div key={answer.answer_id}>
-                        <p>Answerer: {answer.answerer_name} </p>
-                        <p>Answer: {answer.body} </p>
-                        <p>Date: {answer.date} </p>
-                        <p>Helpfulness: {answer.helpfulness} </p>
-                      </div>
-                    )
-                  }
-                  )
-                )
+                  question.data.results.map((answer) => (
+                    <div key={answer.answer_id}>
+                      <p>
+                        Answerer:
+                        {answer.answerer_name}
+                      </p>
+                      <p>
+                        Answer:
+                        {answer.body}
+                      </p>
+                      <p>
+                        Date:
+                        {answer.date}
+                      </p>
+                      <p>
+                        Helpfulness:
+                        {answer.helpfulness}
+                      </p>
+                    </div>
+                  ))
+                );
               }
-            })
-            }
+            })}
           </div>
         </div>
       </div>
-    )
+    );
   }
-
-
-
-
 }
 
 QuestionsAnswers.propTypes = {
   product: PropTypes.instanceOf(Object),
-  questions: PropTypes.instanceOf(Object)
-}
+  questions: PropTypes.instanceOf(Object),
+};
 
 export default QuestionsAnswers;
