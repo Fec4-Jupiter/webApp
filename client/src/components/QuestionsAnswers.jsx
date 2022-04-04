ea28e67014f73e2465a7b71c1072e533828f36e0
Marilene Soares da Costa <lene626@gmail.com>
2022-04-04 14:14:51 -0600

    fixes linter issues in QA.spec and changes css to see QA component

--- client/src/components/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import PropTypes from 'prop-types';
 
 const axios = require('axios');
--- tests/QuestionsAndAnswers.spec.jsx
+++ tests/QuestionsAndAnswers.spec.jsx
@@ -1,22 +1,36 @@
-import QuestionsAnswers from '..client/src/components/QuestionsAnswers.jsx';
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { render, screen, act } from '@testing-library/react';
+import QuestionsAnswers from '../client/src/components/QuestionsAnswers.jsx';
 
-describe('QuestionsAnswers', function() {
-  var {
+// eslint-disable-next-line no-undef
+describe('QuestionsAnswers', () => {
+  const {
     Simulate,
     renderIntoDocument,
     findRendereDOMComponentWithClass,
-    scryRenderedDOMComponentsWithClass
+    scryRenderedDOMComponentsWithClass,
+  // eslint-disable-next-line no-undef
   } = ReactTestUtils;
 
-  var questionsanswers;
+  let qa;
 
-  beforeEach(function() {
-    questionsandanswers = renderIntoDocument(<QuestionsAnswers />);
+  // eslint-disable-next-line no-undef
+  beforeEach(() => {
+    qa = renderIntoDocument(
+      <QuestionsAnswers />,
+    );
   });
 
-  it('should be a stateful class component', function() {
+  // eslint-disable-next-line no-undef
+  it('should be a stateful class component', () => {
+    // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-prototype-builtins
+    // eslint-disable-next-line no-undef
+    // eslint-disable-next-line no-prototype-builtins
+    // eslint-disable-next-line no-undef
     expect(React.Component.isPrototypeOf(QuestionsAnswers)).toEqual(true);
-  })
-
-
+  });
 });
