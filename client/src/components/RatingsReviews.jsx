8ad144aa021cd6d46c8c18877ae02c1ac259e4cc
Jacky <jackylin226@gmail.com>
2022-04-05 17:23:27 -0400

    Edit index => review component don't need reviews data from API Add Sub-component: ReviewList, Review

--- client/src/components/RatingsReviews.jsx
+++ client/src/components/RatingsReviews.jsx
@@ -1,8 +1,63 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-console */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import ReviewList from './RatingsReviews/ReviewList.jsx';
 
-const RatingsReviews = ({currentProduct}) => (
-  <div>RatingsReviews</div>
-)
+class RatingsReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    const { product: { id, name } } = this.props;
+    this.state = {
+      product: { pId: id, pName: name },
+      reviews: [],
+      metadata: {},
+      sort: 'relevance',
+    };
+    this.fetchReviews = this.fetchReviews.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchReviews();
+  }
+
+  // * Fetch all reviews and metadata.
+  // * Init state
+  fetchReviews() {
+    const { product: { pId }, sort } = this.state;
+    axios.get(`/reviews?product_id=${pId}&sort=${sort}`)
+      .then(({ data }) => {
+        // console.log(data);
+        this.setState({
+          reviews: data.results,
+        });
+        return axios.get(`/reviews/meta?product_id=${pId}`);
+      })
+      .then(({ data }) => {
+        // console.log(data);
+        this.setState({
+          metadata: data,
+        });
+      });
+  }
+
+  render() {
+    const { product, reviews, metadata } = this.state;
+    return (
+      <div>
+        <h1>Ratings&Reviews</h1>
+        {reviews.length === 0 ? <p>Loading Reviews....</p>
+          : <ReviewList product={product} reviews={reviews} update={this.fetchReviews} />}
+      </div>
+
+    );
+  }
+}
+
+RatingsReviews.propTypes = {
+  product: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default RatingsReviews;
