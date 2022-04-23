f8a7ea31c6571fed5f843f85cf0118259e98ce41
Kun Chen <justinchen9387@gmail.com>
2022-04-22 22:20:41 -0700

    use router.use instead of router.get, post, and put

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,6 @@ const joshURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc';
 const kunURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc';
 const qiURL = 'http://localhost:3504';
 
-
 // Connect methods to their corresponding routes
 
 // -> shop.com/products/66644/styles
@@ -25,37 +24,11 @@ router.get('/products*', (req, res) => {
     });
 });
 
-router.get('/qa*', (req, res) => {
-  axios.get(kunURL + req.originalUrl, {
-    headers: {
-      Authorization: process.env.API_TOKEN,
-    },
-  })
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-
-router.post('/qa*', (req, res) => {
-  axios.post(kunURL + req.originalUrl, req.body, {
-    headers: {
-      Authorization: process.env.API_TOKEN,
-    },
-  })
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-router.put('/qa*', (req, res) => {
-  axios.put(kunURL + req.originalUrl, {
+router.use('/qa*', (req, res) => {
+  axios({
+    method: req.method,
+    url: kunURL + req.originalUrl,
+    data: req.body,
     headers: {
       Authorization: process.env.API_TOKEN,
     },
@@ -73,7 +46,7 @@ router.use('/reviews*', (req, res) => {
     method: req.method,
     url: qiURL + req.originalUrl,
     data: req.body,
-        headers: {
+    headers: {
       Authorization: process.env.API_TOKEN,
     },
   })
@@ -84,7 +57,5 @@ router.use('/reviews*', (req, res) => {
       res.send(err);
     });
 });
-
-
 
 module.exports = router;
